6f6f04ecc49eb8097d4e4937f3ae99d3204f7961
Yamin Mahdi <yamin_khan@asia.com>
2024-05-28 07:10:51 +0600

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    }
+}
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//}
+
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.3.0")
+}
+
+rootProject.name = "TestAmper"
+include(":src")
+project(":src").projectDir = file(".sub")
